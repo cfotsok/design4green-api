ae6498f8400a4cebf7b93b475b4831e473c7a550
Claude FOTSO <fotso1claude@gmail.com>
2023-11-17 04:17:38 +0100

    cors

--- src/Design4Green/settings.py
+++ src/Design4Green/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -14,7 +13,6 @@ SECRET_KEY = 'django-insecure-3km%e#r)o9rpdi=ca)&@e5rrzg%0&8lj3p#@m=^@(7n%k7dxrm
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -59,8 +57,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Design4Green.wsgi.application'
+CORS_ORIGIN_ALLOW_ALL = True
 
+WSGI_APPLICATION = 'Design4Green.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -71,7 +70,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -91,7 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -102,7 +99,6 @@ TIME_ZONE = 'Europe/Paris'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
